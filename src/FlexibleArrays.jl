1c1cc7613c5b8154c6013b57142a42eb17cdf878
Erik Schnetter <schnetter@gmail.com>
2016-01-26 22:09:16 -0500

    Import code

--- src/FlexibleArrays.jl
+++ src/FlexibleArrays.jl
@@ -1,5 +1,286 @@
 module FlexibleArrays
 
-# package code goes here
+typealias DimSpec NTuple{2, Nullable{Int}}
 
-end # module
+have_lb(ds::DimSpec) = !isnull(ds[1])
+have_ub(ds::DimSpec) = !isnull(ds[2])
+get_lb(ds::DimSpec) = get(ds[1])
+get_ub(ds::DimSpec) = get(ds[2])
+
+export FlexArray
+@generated function FlexArray{T}(::Type{Val{T}})
+    @assert isa(T, Tuple)
+    N = length(T)
+    @assert isa(T, NTuple{N, DimSpec})
+    rank = N
+    dimspecs = T
+
+    have_all_lb = true
+    for n in 1:rank
+        have_all_lb &= have_lb(dimspecs[n])
+    end
+    have_all_ub = true
+    for n in 1:rank
+        have_all_ub &= have_ub(dimspecs[n])
+    end
+    have_all_bnd = have_all_lb && have_all_ub
+    if have_all_bnd
+        size = 1
+        for n in 1:rank
+            size *= max(0, get_ub(dimspecs[n]) - get_lb(dimspecs[n]) + 1)
+        end
+    else
+        size = nothing
+    end
+
+    have_str = Vector{Bool}(rank+1)
+    get_str = Vector{Any}(rank+1)
+    have_str[1] = true
+    get_str[1] = 1
+    for n in 2:rank+1
+        have_str[n] =
+            have_str[n-1] && have_lb(dimspecs[n-1]) && have_ub(dimspecs[n-1])
+        if have_str[n]
+            get_str[n] =
+                get_str[n-1] *
+                max(0, (get_ub(dimspecs[n-1]) - get_lb(dimspecs[n-1]) + 1))
+        else
+            get_str[n] = nothing
+        end
+    end
+
+    have_offset = rank == 0 || have_str[rank] && have_lb(dimspecs[rank])
+    if have_offset
+        get_offset =
+            - sum(Int[get_str[n] * get_lb(dimspecs[n]) for n in 1:rank])
+    else
+        get_offset = nothing
+    end
+
+    decls = []
+
+    # Type declaration
+
+    typename = gensym(:FlexArrayImpl)
+    # @show typename
+    block = []
+    for n in 1:rank
+        # @show dimspecs
+        if have_lb(dimspecs[n])
+            push!(block, :(const $(symbol(:lbnd,n)) = $(get_lb(dimspecs[n]))))
+        else
+            push!(block, :($(symbol(:lbnd,n))::Int))
+        end
+        if have_ub(dimspecs[n])
+            push!(block, :(const $(symbol(:ubnd,n)) = $(get_ub(dimspecs[n]))))
+        else
+            push!(block, :($(symbol(:ubnd,n))::Int))
+        end
+        if have_str[n]
+            push!(block, :(const $(symbol(:str,n)) = $(get_str[n])))
+        else
+            push!(block, :($(symbol(:str,n))::Int))
+        end
+    end
+    if have_str[rank+1]
+        push!(block, :(const size = $(get_str[rank+1])))
+    else
+        push!(block, :(size::Int))
+    end
+    if have_offset
+        push!(block, :(const offset = $(get_offset)))
+    else
+        push!(block, :(offset::Int))
+    end
+    # TODO: use a more low-level array representation
+    push!(block, :(arr::Vector{T}))
+
+    # Constructor
+    funcargs = []
+    for n in 1:rank
+        if !have_lb(dimspecs[n])
+            push!(funcargs, :($(symbol(:lbnd,n))::Int))
+        end
+        if !have_ub(dimspecs[n])
+            push!(funcargs, :($(symbol(:ubnd,n))::Int))
+        end
+    end
+
+    body = []
+    for n in 2:rank
+        if !have_str[n]
+            push!(body,
+                :($(symbol(:str,n)) =
+                    $(symbol(:str,n-1)) *
+                    max(0, $(symbol(:ubnd,n-1)) - $(symbol(:lbnd,n-1)) + 1)))
+        end
+    end
+    if !have_str[rank+1]
+        push!(body,
+            :(size =
+                $(symbol(:str,rank)) *
+                max(0, $(symbol(:ubnd,rank)) - $(symbol(:lbnd,rank)) + 1)))
+    end
+    if !have_offset
+        push!(body,
+            let
+                args = []
+                for n in 1:rank
+                    push!(args, :($(symbol(:str,n)) * $(symbol(:lbnd,n))))
+                end
+                :(offset = - $(Expr(:call, +, 0, args...)))
+            end)
+    end
+    push!(body,
+        let
+            args = []
+            for n in 1:rank
+                if !have_lb(dimspecs[n])
+                    push!(args, :($(symbol(:lbnd,n))))
+                end
+                if !have_ub(dimspecs[n])
+                    push!(args, :($(symbol(:ubnd,n))))
+                end
+                if !have_str[n]
+                    push!(args, :($(symbol(:str,n))))
+                end
+            end
+            if !have_str[rank+1]
+                push!(args, :size)
+            end
+            if !have_offset
+                push!(args, :offset)
+            end
+            Expr(:call, :new, args..., :(Vector{T}(size)))
+        end)
+
+    push!(block,
+        :($(Expr(:call, typename, funcargs...)) = $(Expr(:block, body...))))
+
+    push!(decls, Expr(:type, false, :($typename{T}), Expr(:block, block...)))
+
+    push!(decls, :(Base.eltype{T}(::Type{$typename{T}}) = T))
+    push!(decls, :(Base.eltype{T}(::$typename{T}) = T))
+
+    push!(decls, :(export lbnd))
+    for n in 1:rank
+        if have_lb(dimspecs[n])
+            push!(decls, :(lbnd{T}(::$typename{T}, ::Type{Val{$n}}) =
+                $(get_lb(dimspecs[n]))))
+            push!(decls, :(lbnd{T}(::Type{$typename{T}}, ::Type{Val{$n}}) =
+                $(get_lb(dimspecs[n]))))
+        else
+            push!(decls, :(lbnd{T}(arr::$typename{T}, ::Type{Val{$n}}) =
+                arr.$(symbol(:lbnd,n))))
+        end
+    end
+    push!(decls, :(lbnd{T}(arr::$typename{T}, n::Int) = lbnd(arr, Val{n})))
+    push!(decls, :(lbnd{T}(arr::Type{$typename{T}}, n::Int) =
+        lbnd($typename{T}, Val{n})))
+
+    push!(decls, :(export ubnd))
+    for n in 1:rank
+        if have_ub(dimspecs[n])
+            push!(decls, :(ubnd{T}(::$typename{T}, ::Type{Val{$n}}) =
+                $(get_ub(dimspecs[n]))))
+            push!(decls, :(ubnd{T}(::Type{$typename{T}}, ::Type{Val{$n}}) =
+                $(get_ub(dimspecs[n]))))
+        else
+            push!(decls, :(ubnd{T}(arr::$typename{T}, ::Type{Val{$n}}) =
+                arr.$(symbol(:ubnd,n))))
+        end
+    end
+    push!(decls, :(ubnd{T}(arr::$typename{T}, n::Int) = ubnd(arr, Val{n})))
+    push!(decls, :(ubnd{T}(arr::Type{$typename{T}}, n::Int) =
+        ubnd($typename{T}, Val{n})))
+
+    for n in 1:rank
+        push!(decls, :(Base.size{T}(arr::$typename{T}, ::Type{Val{$n}}) =
+            ubnd(arr, Val{$n}) - lbnd(arr, Val{$n}) + 1))
+        if have_lb(dimspecs[n]) && have_ub(dimspecs[n])
+            push!(decls, :(Base.size{T}(::Type{$typename{T}}, ::Type{Val{$n}}) =
+                ubnd($typename{T}, Val{$n}) - lbnd($typename{T}, Val{$n}) + 1))
+        end
+    end
+    push!(decls, :(Base.size{T}(arr::$typename{T}, n::Int) =
+        size(arr, Val{n})))
+    push!(decls, :(Base.size{T}(arr::Type{$typename{T}}, n::Int) =
+        size($typename{T}, Val{n})))
+
+    if have_all_bnd
+        push!(decls, :(Base.length{T}(::$typename{T}) = $size))
+        push!(decls, :(Base.length{T}(::Type{$typename{T}}) = $size))
+    else
+        push!(decls, :(Base.length{T}(arr::$typename{T}) = arr.size))
+    end
+
+    push!(decls,
+        let
+            funcargs = []
+            for n in 1:rank
+                push!(funcargs, :($(symbol(:ind,n))::Int))
+            end
+            body = []
+            # for n in 1:rank
+            #     # check indices
+            #     push!(body,
+            #         :(@assert lbnd(arr, Val{$n}) <= $(symbol(:ind,n)) <=
+            #             ubnd(arr, Val{$n})))
+            # end
+            args = []
+            if have_offset
+                push!(args, get_offset)
+            else
+                push!(args, :(arr.offset))
+            end
+            for n in 1:rank
+                if have_str[n]
+                    push!(args, :($(get_str[n]) * $(symbol(:ind,n))))
+                else
+                    push!(args, :(arr.$(symbol(:str,n)) * $(symbol(:ind,n))))
+                end
+            end
+            push!(body, :(arr.arr[$(Expr(:call, :+, 1, args...))]))
+            :($(Expr(:call, :(Base.getindex{T}),
+                :(arr::$typename{T}), funcargs...)) =
+                $(Expr(:block, body...)))
+        end)
+
+    push!(decls,
+        let
+            funcargs = []
+            for n in 1:rank
+                push!(funcargs, :($(symbol(:ind,n))::Int))
+            end
+            body = []
+            # for n in 1:rank
+            #     # check indices
+            #     push!(body,
+            #         :(@assert lbnd(arr, Val{$n}) <= $(symbol(:ind,n)) <=
+            #             ubnd(arr, Val{$n})))
+            # end
+            args = []
+            if have_offset
+                push!(args, get_offset)
+            else
+                push!(args, :(arr.offset))
+            end
+            for n in 1:rank
+                if have_str[n]
+                    push!(args, :($(get_str[n]) * $(symbol(:ind,n))))
+                else
+                    push!(args, :(arr.$(symbol(:str,n)) * $(symbol(:ind,n))))
+                end
+            end
+            push!(body, :(arr.arr[$(Expr(:call, :+, 1, args...))] = val))
+            :($(Expr(:call, :(Base.setindex!{T}),
+                :(arr::$typename{T}), :val, funcargs...)) =
+                $(Expr(:block, body...)))
+        end)
+
+    eval(@show Expr(:block, decls...))
+
+    typename
+end
+
+end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,101 @@
 using FlexibleArrays
 using Base.Test
 
-# write your own tests here
-@test 1 == 1
+# 0D
+
+Arr0 = FlexArray(Val{()}){Float64}
+Arr0b = FlexArray(Val{()}){Int}
+@test eltype(Arr0) === Float64
+@test length(Arr0) === 1
+
+arr0 = Arr0()
+@test eltype(arr0) === Float64
+@test length(arr0) === 1
+
+arr0[] = 42
+@test arr0[] === 42.0
+
+# 1D
+
+Arr1_fix = FlexArray(Val{((Nullable{Int}(1), Nullable{Int}(10)),)}){Float64}
+@test eltype(Arr1_fix) === Float64
+@test length(Arr1_fix) === 10
+@test lbnd(Arr1_fix, 1) === 1
+@test ubnd(Arr1_fix, 1) === 10
+@test size(Arr1_fix, 1) === 10
+arr1_fix = Arr1_fix()
+@test eltype(arr1_fix) === Float64
+@test length(arr1_fix) === 10
+@test lbnd(arr1_fix, 1) === 1
+@test ubnd(arr1_fix, 1) === 10
+@test size(arr1_fix, 1) === 10
+for i in 1:10
+    arr1_fix[i] = 42+i
+end
+for i in 1:10
+    @test arr1_fix[i] === 42.0+i
+end
+
+Arr1_lb = FlexArray(Val{((Nullable{Int}(1), Nullable{Int}()),)}){Float64}
+@test eltype(Arr1_lb) === Float64
+@test lbnd(Arr1_fix, 1) === 1
+arr1_lb = Arr1_lb(10)
+@test eltype(arr1_lb) === Float64
+@test length(arr1_lb) === 10
+@test lbnd(arr1_lb, 1) === 1
+@test ubnd(arr1_lb, 1) === 10
+@test size(arr1_lb, 1) === 10
+
+Arr1_gen = FlexArray(Val{((Nullable{Int}(), Nullable{Int}()),)}){Float64}
+@test eltype(Arr1_gen) === Float64
+arr1_gen = Arr1_gen(1,10)
+@test eltype(arr1_gen) === Float64
+@test length(arr1_gen) === 10
+@test lbnd(arr1_gen, 1) === 1
+@test ubnd(arr1_gen, 1) === 10
+@test size(arr1_gen, 1) === 10
+
+# 2D
+
+ds(::Colon) = Nullable{Int}()
+ds(i::Integer) = Nullable{Int}(i)
+ds(t::Tuple) = map(ds, t)
+
+for bnds1 in [(1,10), (:,10), (1,:), (:,:)],
+    bnds2 in [(0,11), (:,11), (0,:), (:,:)]
+
+    ds1 = ds(bnds1)
+    ds2 = ds(bnds2)
+
+    Arr2 = FlexArray(Val{(ds1,ds2)}){Float64}
+    @test eltype(Arr2) === Float64
+    if bnds1[1] !== (:) && bnds1[2] !== (:) && bnds2[1] !== (:) &&
+            bnds2[2] !== (:)
+        @test length(Arr2) === 120
+    end
+    bnds1[1] !== (:) && @test lbnd(Arr2, 1) === 1
+    bnds2[1] !== (:) && @test lbnd(Arr2, 2) === 0
+    bnds1[2] !== (:) && @test ubnd(Arr2, 1) === 10
+    bnds2[2] !== (:) && @test ubnd(Arr2, 2) === 11
+    bnds1[1] !== (:) && bnds1[2] !== (:) && @test size(Arr2, 1) === 10
+    bnds2[1] !== (:) && bnds2[2] !== (:) && @test size(Arr2, 2) === 12
+    sizes = []
+    bnds1[1] === (:) && push!(sizes, 1)
+    bnds1[2] === (:) && push!(sizes, 10)
+    bnds2[1] === (:) && push!(sizes, 0)
+    bnds2[2] === (:) && push!(sizes, 11)
+    arr2 = Arr2(sizes...)
+    @test eltype(arr2) === Float64
+    @test length(arr2) === 120
+    @test lbnd(arr2, 1) === 1
+    @test lbnd(arr2, 2) === 0
+    @test ubnd(arr2, 1) === 10
+    @test ubnd(arr2, 2) === 11
+    @test size(arr2, 1) === 10
+    @test size(arr2, 2) === 12
+
+end
+
+Arr3 = FlexArray(Val{(ds((0,3)), ds((0,:)), ds((0,:)))}){Int}
+arr3 = Arr3(4,5)
+@code_native arr3[1,2,3]
